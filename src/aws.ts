f80903214f40f889630b9f3615fdb3ab1135ce59
DucianX <jeremiahdq@163.com>
2024-12-25 21:00:28 -0800

    done 9.10

--- src/aws.ts
+++ src/aws.ts
@@ -1,5 +1,7 @@
 import AWS from 'aws-sdk';
 import { AWSRegions } from './types/aws';
+import { marshall } from '@aws-sdk/util-dynamodb';
+import { Vendor } from './types/twitter';
 
 AWS.config.update({ region: AWSRegions.US_EAST_1 });
 
@@ -16,7 +18,21 @@ export const dynamodbCreateTable = async (
     const res = await dynamodb.createTable(params).promise();
     console.log('Table created', res);
     return res;
-  } catch (error) {
+  } catch (error: any) {
+    // 如果表已存在，调用 describeTable
+    if (error.code === 'ResourceInUseException') {
+      console.log(`Table already exists. Fetching details for table: ${params.TableName}`);
+      try {
+        const tableDetails = await dynamodb
+          .describeTable({ TableName: params.TableName })
+          .promise();
+        console.log('Table details:', tableDetails);
+        return tableDetails; // 返回表的详细信息
+      } catch (describeError) {
+        console.error('Error describing the table:', describeError);
+        throw new Error('Error describing existing table');
+      }
+    } 
     console.error(error);
     throw new Error('dynamodbCreateTable error');
   }
@@ -34,4 +50,39 @@ export const dynamodbDescribeTable = async (tableName: string) => {
     console.error(error);
     throw new Error('dynamodbDescribeTable error');
   }
+};
+
+// 3 - Delete a table
+export const dynamodbDeleteTable = async (tableName: string) => {
+  try {
+    const res = await dynamodb
+      .deleteTable({ TableName: tableName })
+      .promise();
+    console.log('Table deleted', res);
+    return res;
+  } catch (error) {
+    console.error(error);
+    throw new Error('dynamodbDeleteTable error');
+  }
+};
+// 4 - Insert a record
+export const dynamodbCreateRecord = async (
+  tableName: string,
+  vendorData: Vendor
+) => {
+  try {
+    const res = await dynamodb
+      .putItem({ TableName: tableName, Item: marshall(vendorData) })
+      .promise();
+    console.log('Record created', res);
+    return res;
+  } catch (error) {
+    if (error instanceof Error) {
+      return error;
+    }
+    console.error(error);
+    throw new Error('dynamodbCreateRecord error');
+  }
+
+  
 };
--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,13 @@
 
-import { dynamodbCreateTable, dynamodbDescribeTable } from './aws';
+import { dynamodbCreateTable, dynamodbDeleteTable, dynamodbDescribeTable, dynamodbCreateRecord } from './aws';
+import vendors from './data/vendors';
 
 const init = async () => {
-  const TABLE_NAME_CONST = 'vendors';
+  const TABLE_NAME_CONST = 'vendors_Test';
 
   const vendorsTestTableParams: AWS.DynamoDB.CreateTableInput = {
     TableName: TABLE_NAME_CONST,
+    // PrimaryKey:twiId HASH：表示分区键（Partition Key），决定数据的存储分区位置。
     KeySchema: [{ AttributeName: 'twitterId', KeyType: 'HASH' }],
     AttributeDefinitions: [
       { AttributeName: 'twitterId', AttributeType: 'S' },
@@ -17,10 +19,25 @@ const init = async () => {
   };
 
   // 1 - create table
-    // dynamodbCreateTable(vendorsTestTableParams);
+  //   dynamodbCreateTable(vendorsTestTableParams);
 
   // 2 - describe table
-    dynamodbDescribeTable(TABLE_NAME_CONST);
+  //   dynamodbDescribeTable(TABLE_NAME_CONST);
+
+  // 3 - delete table
+  //   dynamodbDeleteTable(TABLE_NAME_CONST);
+
+  // 4 - Insert a record
+  // const DUMMY_DATA = vendors[0];
+  // dynamodbCreateRecord(TABLE_NAME_CONST, DUMMY_DATA);
+
+  // 5 - Seed all data
+  for (const vendorData of vendors) {
+    const res = dynamodbCreateRecord(TABLE_NAME_CONST, vendorData);
+    if (res instanceof Error) {
+      console.log('Error', vendorData, res);
+    }
+  }
 };
 
 init();
